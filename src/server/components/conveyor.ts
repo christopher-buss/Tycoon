b07d958ee2ff0fc9c20fdf3cb9a31b0f717a35aa
christopher-buss <christopher.buss+github@protonmail.com>
2022-09-13 05:30:15 +0100

    Changed conveyor to use speed attribute

--- src/server/components/conveyor.ts
+++ src/server/components/conveyor.ts
@@ -1,21 +1,29 @@
 import { BaseComponent, Component } from "@flamework/components";
 import { OnStart } from "@flamework/core";
 
+interface IConveyorAttributes {
+	ConveyorSpeed: number;
+}
+
 interface IConveyor extends BasePart {
 	AttStart: Attachment;
 	AttEnd: Attachment;
 }
 
+/**
+ * Sets up the velocity of a given conveyor.
+ *
+ * This is only used as an initial setup, therefore the component is destroyed
+ * once setup is complete.
+ */
 @Component({
 	tag: "ConveyorBelt",
 })
-export class conveyor extends BaseComponent<{}, IConveyor> implements OnStart {
-	private readonly conveyorSpeed = 22;
-
+export class conveyor extends BaseComponent<IConveyorAttributes, IConveyor> implements OnStart {
 	public onStart(): void {
 		const direction = this.instance.AttEnd.WorldPosition.sub(this.instance.AttStart.WorldPosition);
 
-		const velocity = direction.Unit.mul(this.conveyorSpeed);
+		const velocity = direction.Unit.mul(this.attributes.ConveyorSpeed);
 		this.instance.AssemblyLinearVelocity = velocity;
 
 		this.destroy();
